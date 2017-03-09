7de71d4d6140e309b129889423c06188ed1fd266
ubuntu <ubuntu@saurabhubuntu.ezdi.local>
2017-03-09 13:10:01 +0530

    Pattern find z algorithm solution

---
+++ NAJPF_Z.cpp
@@ -0,0 +1,121 @@
+/* user :saurabh3240
+    problem:  NAJPF
+    date: 04-03-2016
+*/
+ 
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <string.h>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <limits>
+#include <string>
+#include <cassert>
+ 
+using namespace std;
+typedef long long LL;
+typedef pair<int,int> pii;
+ 
+#define forup(i,a,b) for(int i=a; i<b; ++i)
+#define fordn(i,a,b) for(int i=a; i>b; --i)
+#define rep(i,a) for(int i=0; i<a; ++i)
+ 
+#define dforup(i,a,b) for(i=a; i<b; ++i)
+#define dfordn(i,a,b) for(i=a; i>b; --i)
+#define drep(i,a) for(i=0; i<a; ++i)
+ 
+#define slenn(s,n) for(n=0; s[n]!=13 and s[n]!=0; ++n);s[n]=0
+ 
+#define gi(x) scanf("%d",&x)
+#define gl(x) scanf("%lld",&x)
+#define gd(x) scanf("%lf",&x)
+#define gs(x) scanf("%s",x)
+ 
+#define pis(x) printf("%d ",x)
+#define pin(x) printf("%d\n",x)
+#define pls(x) printf("%lld ",x)
+#define pln(x) printf("%lld\n",x)
+#define pds(x) printf("%.12f ",x)
+#define pdn(x) printf("%.12f\n",x)
+#define pnl() printf("\n")
+ 
+#define fs first
+#define sc second
+#define ll long long
+#define pb push_back
+#define MOD 1000000007
+#define limit 1000006
+ 
+ 
+string needle,haystack;
+
+
+
+
+vector<int> z_function(string s) {
+    int n = (int) s.length();
+    vector<int> z(n);
+    for (int i = 1, l = 0, r = 0; i < n; ++i) {
+        if (i <= r)
+            z[i] = min (r - i + 1, z[i - l]);
+        while (i + z[i] < n && s[z[i]] == s[i + z[i]])
+            ++z[i];
+        if (i + z[i] - 1 > r)
+            l = i, r = i + z[i] - 1;
+    }
+    return z;
+}
+
+int main()
+{	
+	int t;
+	gi(t);
+	
+	
+	while(t--)
+	{
+		cin>> haystack;
+		cin>> needle;
+		haystack=needle+"#"+haystack;
+		vector<int> v  = z_function(haystack);
+		vector<int> temp;
+		int m = needle.size();
+		rep(i,v.size())
+		{
+			if(v[i]==m)
+				temp.pb(i-m);
+		}
+		if(temp.size()==0)
+		{
+			printf("Not Found\n\n");
+		}
+		else
+		{
+			pin(temp.size());
+			rep(j,temp.size())
+			{
+				pis(temp[j]);	
+			}	
+			printf("\n\n");
+		}
+		
+	}
+		
+}
+ 
+ 
