76446d6b1f87c4aa02ac9f7ad58dcd5caebb215a
saurabhj-ezdi <saurabh.j@ezdi.us>
2018-02-15 04:56:21 +0530

    segment Tree best; medium

--- GSS1.cpp
+++ GSS1.cpp
@@ -4,7 +4,7 @@
     platform :hackerrank
     date: 22-06-2015
 */
- 
+
 #include <vector>
 #include <list>
 #include <map>
@@ -28,26 +28,26 @@
 #include <limits>
 #include <string>
 #include <cassert>
- 
+
 using namespace std;
 typedef long long LL;
 typedef pair<int,int> pii;
- 
+
 #define forup(i,a,b) for(int i=a; i<b; ++i)
 #define fordn(i,a,b) for(int i=a; i>b; --i)
 #define rep(i,a) for(int i=0; i<a; ++i)
- 
+
 #define dforup(i,a,b) for(i=a; i<b; ++i)
 #define dfordn(i,a,b) for(i=a; i>b; --i)
 #define drep(i,a) for(i=0; i<a; ++i)
- 
+
 #define slenn(s,n) for(n=0; s[n]!=13 and s[n]!=0; ++n);s[n]=0
- 
+
 #define gi(x) scanf("%d",&x)
 #define gl(x) scanf("%lld",&x)
 #define gd(x) scanf("%lf",&x)
 #define gs(x) scanf("%s",x)
- 
+
 #define pis(x) printf("%d ",x)
 #define pin(x) printf("%d\n",x)
 #define pls(x) printf("%lld ",x)
@@ -55,7 +55,7 @@ typedef pair<int,int> pii;
 #define pds(x) printf("%.12f ",x)
 #define pdn(x) printf("%.12f\n",x)
 #define pnl() printf("\n")
- 
+
 #define fs first
 #define sc second
 #define ll long long
@@ -63,108 +63,106 @@ typedef pair<int,int> pii;
 #define MOD 1000000007
 #define limit 10000005
 
-int readint()
-    {
-       int cc = getc(stdin);
-       for (;cc < '0' || cc > '9';)  cc = getc(stdin);
-       int ret = 0;
-       for (;cc >= '0' && cc <= '9';)
-       {
-          ret = ret * 10 + cc - '0';
-          cc = getc(stdin);
-       }
-      return ret;
-   }
 
+// Classic Segment Tree
 
-unsigned long long mod_pow(unsigned long long num, unsigned long long pow, unsigned long long mod)
+int a[50005];
+struct node
 {
-    unsigned long long test;
-    unsigned long long n = num;
-    for(test = 1; pow; pow >>= 1)
-    {
-        if (pow & 1)
-            test = ((test % mod) * (n % mod)) % mod;
-        n = ((n % mod) * (n % mod)) % mod;
-    }
-    return test; /* note this is potentially lossy */
-}
+    int pre;
+    int mx;
+    int total;
+    int suff;
+};
+struct node s[4*50005];
+int n;
+void build(int id = 1,int l = 0,int r = n)         //root index 1,children 2x,2x+1
+{						   // root = [0,n)
+	if( l+1 == r )
+	{
+	    s[id].pre = a[l];			// put values in leaf nodes
+        s[id].mx = a[l];			// put values in leaf nodes
+        s[id].total = a[l];			// put values in leaf nodes
+        s[id].suff = a[l];			// put values in leaf nodes
 
-ll gcd(ll a,ll b)
-{	ll r;
-	while(b)
-	{	r= a%b;
-		a = b;
-		b = r;
+        return;				// comes from array
 	}
-	return a;
-}
 
-int RMQUtil(int *st, int ss, int se, int qs, int qe, int index)
-{
-    if (qs <= ss && qe >= se)
-        return st[index];
- 
-    if (se < qs || ss > qe)
-        return -1;
- 
-    int mid = ss + (se -ss)/2;
-    return min( RMQUtil(st, ss, mid, qs, qe, 2*index+1),
-                  RMQUtil(st, mid+1, se, qs, qe, 2*index+2));
+	ll mid = l + (r-l)/2;
+	build( id*2, l, mid);			// interval = [left,right)
+	build( id*2+1, mid, r);
+
+    s[id].pre= max(s[id * 2].total+s[id*2+1].pre,s[id*2].pre);
+	s[id].total= s[id * 2].total + s[id * 2 + 1].total;	// put values in non-leaf nodes
+    s[id].suff= max(s[id * 2+1].total+s[id*2].suff,s[id*2+1].suff);
+
+    s[id].mx = max(max(s[id*2].mx, s[id*2+1].mx),s[id*2].suff+ s[id*2+1].pre);
+						// comes from child nodes
 }
- 
-int RMQ(int *st, int n, int qs, int qe)
-{
-    if (qs < 0 || qe > n-1 || qs > qe)
-    {
-        printf("Invalid Input");
-        return -1;
-    }
- 
-    return RMQUtil(st, 0, n-1, qs, qe, 0);
+// void modify(int p, int x, int id=1, int l=0, int r=n)    // call modify(p,x)
+// {
+// 				    //x is the value to which a[p] is changed
+// 	s[id] += x-a[p] ;  		// change the value of the segtree node
+// 	if( l+1 == r){			// l=r-1=p
+// 	   a[p]=x;                 // change the value in the array
+// 	   return;
+// 	}
+// 	ll mid = l + (r-l)/2;
+//
+// 	if( p < mid )
+// 	{
+// 	   modify(p, x, id*2, l, mid);
+// 	}
+// 	else
+// 	{
+// 	   modify(p, x, id*2+1, mid, r);
+// 	}
+//
+// }
+struct node query(int x,int y,int id=1,int l=0,int r=n){		//verify return type
+							// call query(l,r)
+    struct node zero;
+    zero = {-MOD,-MOD,0,-MOD};
+    // cout<<x<<" "<<y<<"-"<<id<<"-"<<l<<"-"<<r<<endl;
+	if( x >= r or l >= y )  return zero;	// no overlap
+	if( x <= l and r <= y )  return s[id];      // complete overlap
+	ll mid = l + (r-l)/2;			  // partial overlap
+    struct node n1,n2,n3;
+    n1 = query(x, y, id*2, l, mid) ;
+    n2 = query(x, y, id*2+1, mid, r);
+
+    n3.pre= max(n1.total+n2.pre,n1.pre);
+	n3.total= n1.total + n2.total;	// put values in non-leaf nodes
+    n3.suff= max(n2.total+n1.suff,n2.suff);
+    n3.mx =  max(  max(n1.mx, n2.mx),n1.suff+ n2.pre  );
+	return n3;
 }
- 
-int constructSTUtil(int arr[], int ss, int se, int *st, int si)
+void print_node(struct node n)
 {
-    if (ss == se)
-    {
-        st[si] = arr[ss];
-        return arr[ss];
-    }
- 
-    int mid = ss + (se -ss)/2;
-    st[si] =  min(constructSTUtil(arr, ss, mid, st, si*2+1),
-                     constructSTUtil(arr, mid+1, se, st, si*2+2));
-    return st[si];
-}
- 
-int *constructST(int arr[], int n)
-{
-    int x = (int)(ceil(log2(n))); //Height of segment tree
-    int max_size = 2*(int)pow(2, x) - 1; //Maximum size of segment tree
-    int *st = new int[max_size];
-    constructSTUtil(arr, 0, n-1, st, 0); 
-    return st;
+    cout<<n.pre<<" "<<n.mx<<" "<<n.total<<" "<<n.suff<<endl;
 }
 int main()
 {
-	int n;
 	gi(n);
-	int arr[n];
 	rep(i,n)
 	{
-		gi(arr[i]);	
+		gi(a[i]);
 	}
-	int *st = constructST(arr,n);
+    build();
+    // rep(i,n*2+1)
+    // {
+    //     print_node(s[i]);
+    // }
+
 	int q;
 	gi(q);
 	int x,y;
-		
+
 	while(q--)
 	{	gi(x);
 		gi(y);
-		int ans =RMQ(st,n,x,y); 
-		pin(ans);
-		
+        struct node n = query(x-1,y);
+		pin(n.mx);
+
 	}
 }
