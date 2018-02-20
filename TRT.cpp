65eebd5daa9648a02a278d9df73d74865f60fbf8
saurabhj-ezdi <saurabh.j@ezdi.us>
2018-02-20 16:57:00 +0530

    DP solution GOOD

---
+++ TRT.cpp
@@ -0,0 +1,95 @@
+    #include <bits/stdc++.h>
+    #include <cstdio>
+    #include <cstring>
+    #include <cmath>
+    #include <cstring>
+    #include <chrono>
+    #include <complex>
+    #define endl "\n"
+    #define ll long long int
+    #define vi vector<int>
+    #define vll vector<ll>
+    #define vvi vector < vi >
+    #define pii pair<int,int>
+    #define pll pair<long long, long long>
+    #define mod 1000000007
+    #define limit 100005
+    #define inf 1000000000000000001;
+    #define all(c) c.begin(),c.end()
+    #define mp(x,y) make_pair(x,y)
+    #define mem(a,val) memset(a,val,sizeof(a))
+    #define eb emplace_back
+    #define forup(i,a,b) for(int i=a; i<b; ++i)
+    #define fordn(i,a,b) for(int i=a; i>b; --i)
+    #define rep(i,a) for(int i=0; i<a; ++i)
+    #define dforup(i,a,b) for(i=a; i<b; ++i)
+    #define dfordn(i,a,b) for(i=a; i>b; --i)
+    #define drep(i,a) for(i=0; i<a; ++i)
+    #define gi(x) fastRead_int(x)
+    #define gl(x) scanf("%lld",&x)
+    #define gd(x) scanf("%lf",&x)
+    #define gs(x) scanf("%s",x)
+    #define pis(x) printf("%d ",x)
+    #define pin(x) printf("%d\n",x)
+    #define pls(x) printf("%lld ",x)
+    #define pln(x) printf("%lld\n",x)
+    #define pds(x) printf("%.12f ",x)
+    #define pdn(x) printf("%.12f\n",x)
+    #define pnl() printf("\n")
+    #define fs first
+    #define sc second
+    #define pb push_back
+    using namespace std;
+    int dp[2002][2002];
+    int n;
+    int arr[2002];
+    int ans =0;
+    inline void fastRead_int(int &x) {
+        register int c = getchar();
+        x = 0;
+        int neg = 0;
+
+        for(; ((c<48 || c>57) && c != '-'); c = getchar());
+
+        if(c=='-') {
+            neg = 1;
+            c = getchar();
+        }
+
+        for(; c>47 && c<58 ; c = getchar()) {
+            x = (x<<1) + (x<<3) + c - 48;
+        }
+
+        if(neg)
+            x = -x;
+    }
+    int call(int l,int r)
+    {
+        int t = n-r+l;
+        if(l>r)
+            return 0;
+        if(l==r)
+        {
+            dp[l][r]=t*arr[l];
+            return dp[l][r];
+        }
+        if(dp[l][r]!=-1)
+            return dp[l][r];
+        else
+        {     int x = max(t*arr[l]+call(l+1,r),t*arr[r]+call(l,r-1));
+            dp[l][r]=x;
+                return x;
+        }
+    }
+    int main()
+    {
+        rep(i,2002)
+        {
+            rep(j,2002)
+                dp[i][j]=-1;
+        }
+        gi(n);
+        rep(i,n)
+            gi(arr[i]);
+        cout<<call(0,n-1)<<endl;;
+    }
