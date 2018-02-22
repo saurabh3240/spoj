e2a2e50ee1db64fb148863ce85ec0f4db8ce30e3
saurabhj-ezdi <saurabh.j@ezdi.us>
2018-02-22 14:22:19 +0530

    sliding window simple

---
+++ HOTELS.cpp
@@ -0,0 +1,97 @@
+
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <string.h>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <limits>
+
+#include <string>
+#include <cassert>
+
+using namespace std;
+typedef long long LL;
+typedef pair<int,int> pii;
+
+#define forup(i,a,b) for(int i=a; i<b; ++i)
+#define fordn(i,a,b) for(int i=a; i>b; --i)
+#define rep(i,a) for(int i=0; i<a; ++i)
+
+#define dforup(i,a,b) for(i=a; i<b; ++i)
+#define dfordn(i,a,b) for(i=a; i>b; --i)
+#define drep(i,a) for(i=0; i<a; ++i)
+
+#define slenn(s,n) for(n=0; s[n]!=13 and s[n]!=0; ++n);s[n]=0
+
+#define gi(x) scanf("%d",&x)
+#define gl(x) scanf("%lld",&x)
+#define gd(x) scanf("%lf",&x)
+#define gs(x) scanf("%s",x)
+
+#define pis(x) printf("%d ",x)
+#define pin(x) printf("%d\n",x)
+#define pls(x) printf("%lld ",x)
+#define pln(x) printf("%lld\n",x)
+#define pds(x) printf("%.12f ",x)
+#define pdn(x) printf("%.12f\n",x)
+#define pnl() printf("\n")
+
+#define fs first
+#define sc second
+#define ll long long
+#define pb push_back
+#define MOD 1000000007
+#define limit 10000005
+#define INF 1000000000
+#define ull unsigned long long
+using namespace std;
+
+int main()
+{
+    int n;
+    gi(n);
+    ll m;
+    gl(m);
+    ll arr[n];
+    rep(i,n)
+        gl(arr[i]);
+    int st=0;
+    int end=-1;
+    ll cursum=0;
+    ll ansmx =0;
+    while(end<n)
+    {
+        //cout<<st<<" "<<end<<" "<<endl;
+        if(cursum<=m)
+        {
+            ansmx = max(ansmx,cursum);
+            end++;
+            cursum+=arr[end];
+        }
+        else
+        {
+            cursum-=arr[st];
+            st++;
+        }
+        if(st>end)
+        {
+            end = st;
+        }
+    }
+    pln(ansmx);
+}
