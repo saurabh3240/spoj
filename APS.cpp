044098a0b350eb6e57653d873b78ad1f97cd77ee
saurabh3240 <saurabh3240@github.com>
2018-03-24 00:15:44 +0530

    number theory prime number

--- APS.cpp
+++ APS.cpp
@@ -1,10 +1,4 @@
-/* user :saurabh3240
-    contest : codechef march long
-    problem:  STRPALIN
-    platform :Codechef
-    date: 06-03-2016
-*/
- 
+
 #include <vector>
 #include <list>
 #include <map>
@@ -28,26 +22,26 @@
 #include <limits>
 #include <string>
 #include <cassert>
- 
+
 using namespace std;
 typedef long long LL;
 typedef pair<int,int> pii;
- 
+
 #define forup(i,a,b) for(int i=a; i<b; ++i)
 #define fordn(i,a,b) for(int i=a; i>b; --i)
 #define rep(i,a) for(int i=0; i<a; ++i)
- 
+
 #define dforup(i,a,b) for(i=a; i<b; ++i)
 #define dfordn(i,a,b) for(i=a; i>b; --i)
 #define drep(i,a) for(i=0; i<a; ++i)
- 
+
 #define slenn(s,n) for(n=0; s[n]!=13 and s[n]!=0; ++n);s[n]=0
- 
+
 #define gi(x) scanf("%d",&x)
 #define gl(x) scanf("%lld",&x)
 #define gd(x) scanf("%lf",&x)
 #define gs(x) scanf("%s",x)
- 
+
 #define pis(x) printf("%d ",x)
 #define pin(x) printf("%d\n",x)
 #define pls(x) printf("%lld ",x)
@@ -55,7 +49,7 @@ typedef pair<int,int> pii;
 #define pds(x) printf("%.12f ",x)
 #define pdn(x) printf("%.12f\n",x)
 #define pnl() printf("\n")
- 
+
 #define fs first
 #define sc second
 #define ll long long
@@ -79,14 +73,14 @@ int main()
 		}
 		ans[i]=ans[i-1]+prime[i];
 	}
-	
+
 	int t;
 	gi(t);
 	while(t--)
 	{
 		int x;
 		gi(x);
-		pln(ans[x]);	
+		pln(ans[x]);
 	}
-	
+
 }
